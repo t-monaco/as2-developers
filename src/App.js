c21de99e6161ebd26506ef8d5911f1cd7f25829d
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-05-01 15:48:04 -0300

    Fixed scroll to top when new location

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    // Redirect,
-    Switch,
-} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavbarRB from './components/navbar/navbar-rb.component';
 import Homepage from './pages/homepage/homepage.component';
 import Project from './pages/project/project.component';
 
 const App = () => {
     return (
-        <Router>
-            <div id='page-container'>
-                <NavbarRB/>
-                <Switch>
-                    <Route exact path='/' component={Homepage}></Route>
-                    <Route path='/:projectId' component={Project}></Route>
-                </Switch>
-            </div>
-        </Router>
+        <div id='page-container'>
+            <NavbarRB />
+            <Switch>
+                <Route exact path='/' component={Homepage}></Route>
+                <Route path='/:projectId' component={Project}></Route>
+            </Switch>
+        </div>
     );
 };
 
