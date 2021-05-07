ab34cb42d9103f8c87641d491c72219966e1a82b
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-05-06 23:01:12 -0300

    Update property context

--- src/contexts/properties/properties.data.js
+++ src/contexts/properties/properties.data.js
@@ -1,13 +1,13 @@
 import uuid from 'react-uuid';
 
-const PROPERTIES_DATA = {
-    sucre: {
+const PROPERTIES_DATA = [
+    {
         id: uuid(),
         name: 'SUCRE',
         linkName: 'sucre',
         heroImage: 'images/sucre/heroImage/heroImage_01.jpg',
-        category: 'TOUCH',
-        status: 'finished',
+        category: 'touch',
+        status: 'finalizado',
         residence: {
             imagesUrl: [
                 'images/sucre/residence/residence_01.jpg',
@@ -37,22 +37,21 @@ const PROPERTIES_DATA = {
         logo: 'images/sucre/logo/logo_01.png',
     },
 
-    bilbao: {
+    {
         id: uuid(),
         name: 'BILBAO',
         linkName: 'bilbao',
         heroImage: 'images/bilbao/heroImage/heroImage_01.jpg',
-        category: 'TOUCH',
-        status: 'finished',
+        category: 'touch',
+        status: 'finalizado',
         residence: {
             imagesUrl: [
                 'images/bilbao/residence/residence_01.jpg',
                 'images/bilbao/residence/residence_02.jpg',
             ],
             text: [
-                'this will be the first paragraph',
-                'And this will be the second',
-                'And this the third, that is why it is an Array :)',
+                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
             ],
         },
         amenities: {
@@ -61,9 +60,8 @@ const PROPERTIES_DATA = {
                 'images/bilbao/amenities/amenities_02.jpg',
             ],
             text: [
-                'this will be the first paragraph',
-                'And this will be the second',
-                'And this the third, that is why it is an Array :)',
+                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
             ],
         },
         location: {
@@ -75,22 +73,21 @@ const PROPERTIES_DATA = {
         logo: 'images/bilbao/logo/logo_01.png',
     },
 
-    superi: {
+    {
         id: uuid(),
         name: 'SUPERI',
         linkName: 'superi',
         heroImage: 'images/superi/heroImage/heroImage_01.jpg',
-        category: 'TOUCH',
-        status: 'finished',
+        category: 'touch',
+        status: 'en construccion',
         residence: {
             imagesUrl: [
                 'images/superi/residence/residence_01.jpg',
                 'images/superi/residence/residence_02.jpg',
             ],
             text: [
-                'this will be the first paragraph',
-                'And this will be the second',
-                'And this the third, that is why it is an Array :)',
+                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
             ],
         },
         amenities: {
@@ -99,9 +96,8 @@ const PROPERTIES_DATA = {
                 'images/superi/amenities/amenities_02.jpg',
             ],
             text: [
-                'this will be the first paragraph',
-                'And this will be the second',
-                'And this the third, that is why it is an Array :)',
+                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
             ],
         },
         location: {
@@ -113,22 +109,21 @@ const PROPERTIES_DATA = {
         logo: 'images/superi/logo/logo_01.png',
     },
 
-    lib_5851: {
+    {
         id: uuid(),
         name: 'LIB 5851',
         linkName: 'lib_5851',
         heroImage: 'images/lib_5851/heroImage/heroImage_01.jpg',
-        category: 'TOUCH',
-        status: 'finished',
+        category: 'touch',
+        status: 'futuro lanzamiento',
         residence: {
             imagesUrl: [
                 'images/lib_5851/residence/residence_01.jpg',
                 'images/lib_5851/residence/residence_02.jpg',
             ],
             text: [
-                'this will be the first paragraph',
-                'And this will be the second',
-                'And this the third, that is why it is an Array :)',
+                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
             ],
         },
         amenities: {
@@ -137,9 +132,8 @@ const PROPERTIES_DATA = {
                 'images/lib_5851/amenities/amenities_02.jpg',
             ],
             text: [
-                'this will be the first paragraph',
-                'And this will be the second',
-                'And this the third, that is why it is an Array :)',
+                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
             ],
         },
         location: {
@@ -150,6 +144,5 @@ const PROPERTIES_DATA = {
         thumbnail: 'images/lib_5851/thumbnail/thumbnail_01.jpg',
         logo: 'images/lib_5851/logo/logo_01.png',
     },
-};
-
+];
 export default PROPERTIES_DATA;
