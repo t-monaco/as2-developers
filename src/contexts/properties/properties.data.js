e61433aa1fdcc511d9bd6f12a0fe964ec46ea8ae
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-05-11 09:43:07 -0300

    Added blueprint context

--- src/contexts/properties/properties.data.js
+++ src/contexts/properties/properties.data.js
@@ -35,6 +35,41 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/sucre/thumbnail/thumbnail_01.jpg',
         logo: 'images/sucre/logo/logo_01.png',
+        blueprint: [
+            {
+                name: 'piso_01_unidad_01',
+                // imageUrl:
+                //     'images/sucre/blueprint/blueprint_piso_01_unidad_01.jpg',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
+                squareMeters: 44,
+            },
+            {
+                name: 'piso_01_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
+                squareMeters: 48,
+            },
+            {
+                name: 'piso_02_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
+                squareMeters: 56,
+            },
+            {
+                name: 'piso_03_unidad_03',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
+                squareMeters: 78,
+            },
+            {
+                name: 'piso_11_unidad_01',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
+                squareMeters: 90,
+            },
+            // documents/sucre/blueprint/sucre_planos_35m2.pdf
+        ],
     },
 
     {
@@ -71,6 +106,41 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/bilbao/thumbnail/thumbnail_01.jpg',
         logo: 'images/bilbao/logo/logo_01.png',
+        blueprint: [
+            {
+                name: 'piso_01_unidad_01',
+                // imageUrl:
+                //     'images/bilbao/blueprint/blueprint_piso_01_unidad_01.jpg',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
+                squareMeters: 44,
+            },
+            {
+                name: 'piso_01_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
+                squareMeters: 48,
+            },
+            {
+                name: 'piso_02_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
+                squareMeters: 56,
+            },
+            {
+                name: 'piso_03_unidad_03',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
+                squareMeters: 78,
+            },
+            {
+                name: 'piso_11_unidad_01',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
+                squareMeters: 90,
+            },
+            // documents/bilbao/blueprint/bilbao_planos_35m2.pdf
+        ],
     },
 
     {
@@ -107,6 +177,41 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/superi/thumbnail/thumbnail_01.jpg',
         logo: 'images/superi/logo/logo_01.png',
+        blueprint: [
+            {
+                name: 'piso_01_unidad_01',
+                // imageUrl:
+                //     'images/superi/blueprint/blueprint_piso_01_unidad_01.jpg',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
+                squareMeters: 44,
+            },
+            {
+                name: 'piso_01_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
+                squareMeters: 48,
+            },
+            {
+                name: 'piso_02_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
+                squareMeters: 56,
+            },
+            {
+                name: 'piso_03_unidad_03',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
+                squareMeters: 78,
+            },
+            {
+                name: 'piso_11_unidad_01',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
+                squareMeters: 90,
+            },
+            // documents/superi/blueprint/superi_planos_35m2.pdf
+        ],
     },
 
     {
@@ -143,6 +248,41 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/lib_5851/thumbnail/thumbnail_01.jpg',
         logo: 'images/lib_5851/logo/logo_01.png',
+        blueprint: [
+            {
+                name: 'piso_01_unidad_01',
+                // imageUrl:
+                //     'images/lib_5851/blueprint/blueprint_piso_01_unidad_01.jpg',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
+                squareMeters: 44,
+            },
+            {
+                name: 'piso_01_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
+                squareMeters: 48,
+            },
+            {
+                name: 'piso_02_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
+                squareMeters: 56,
+            },
+            {
+                name: 'piso_03_unidad_03',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
+                squareMeters: 78,
+            },
+            {
+                name: 'piso_11_unidad_01',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
+                squareMeters: 90,
+            },
+            // documents/lib_5851/blueprint/lib_5851_planos_35m2.pdf
+        ],
     },
 ];
 export default PROPERTIES_DATA;
