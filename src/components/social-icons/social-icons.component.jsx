ee5ef4e69922e492ae68e446fc2aa6f34b98625b
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-01 18:10:32 -0300

    Added whatsapp icon

--- src/components/social-icons/social-icons.component.jsx
+++ src/components/social-icons/social-icons.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {ReactComponent as FacebookIcon} from './../../assets/facebook-logo.svg'
 import {ReactComponent as TwitterIcon} from './../../assets/twitter-logo.svg'
 import {ReactComponent as InstagramIcon} from './../../assets/instagram-logo.svg'
-import {ReactComponent as ASIcon} from './../../assets/as--logo.svg'
+import {ReactComponent as ASIcon} from './../../assets/as-logo.svg'
+import {ReactComponent as WhatsappIcon} from './../../assets/whatsapp-logo.svg'
 import './social-icons.styles.scss'
 
 const SocialIcons = () => {
@@ -37,6 +38,16 @@ const SocialIcons = () => {
                         rel='noopener noreferrer'
                     >
                         <TwitterIcon className='icon' />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='icon-link'
+                        target='_blank'
+                        href='https://wa.me/5491155857777'
+                        rel='noopener noreferrer'
+                    >
+                        <WhatsappIcon className='icon' />
                     </a>
                 </li>
                 <li>
