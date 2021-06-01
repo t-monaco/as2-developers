7cdd63ee8ce739ef726a25a52d74fcf1bae15410
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-01 18:10:16 -0300

    Updated context

--- src/contexts/properties/properties.data.js
+++ src/contexts/properties/properties.data.js
@@ -9,29 +9,33 @@ const PROPERTIES_DATA = [
         category: 'touch',
         status: 'finalizado',
         residence: {
+            title: 'Residencias',
             imagesUrl: [
                 'images/sucre/residence/residence_01.jpg',
                 'images/sucre/residence/residence_02.jpg',
+                'images/sucre/residence/residence_03.jpg',
             ],
             text: [
-                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Hall en doble altura, Unidades desde 37 hasta 100 m2',
+                'La mejor zona de Belgrano R',
+                '2 y 3 ambientes, Duplex',
+                'Departamentos con parrilla propia',
             ],
         },
         amenities: {
+            title: 'Amenities',
             imagesUrl: [
                 'images/sucre/amenities/amenities_01.jpg',
                 'images/sucre/amenities/amenities_02.jpg',
             ],
             text: [
-                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Parrillas – Piscina – Balcones Terrazas – SUM – Bauleras – Laundry -  Sector de Juegos para niños – Cocheras cubiertas',
             ],
         },
         location: {
             lat: -34.57428,
             lng: -58.47288,
-            address: 'Avenida del Libertador 5851, CABA',
+            address: 'Mariscal Antonio José de Sucre 4245 - CABA',
         },
         thumbnail: 'images/sucre/thumbnail/thumbnail_01.jpg',
         logo: 'images/sucre/logo/logo_01.png',
@@ -80,29 +84,22 @@ const PROPERTIES_DATA = [
         category: 'touch',
         status: 'finalizado',
         residence: {
+            title: 'Residencias',
             imagesUrl: [
-                'images/bilbao/residence/residence_01.jpg',
                 'images/bilbao/residence/residence_02.jpg',
+                'images/bilbao/residence/residence_03.jpg',
+                'images/bilbao/residence/residence_01.jpg',
+                'images/bilbao/residence/residence_04.jpg',
             ],
             text: [
-                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-            ],
-        },
-        amenities: {
-            imagesUrl: [
-                'images/bilbao/amenities/amenities_01.jpg',
-                'images/bilbao/amenities/amenities_02.jpg',
-            ],
-            text: [
-                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Modernos y amplios departamentos  en la mejor ubicación. Muy Luminosos con vista a la Plaza de la Misericordia. A 100 m del centro comercial de Av. Directorio.',
+                'Estacionamiento privado y Bauleras.',
             ],
         },
         location: {
             lat: -34.63293,
             lng: -58.45538,
-            address: 'Avenida del Libertador 5851, CABA',
+            address: 'Av. Francisco Bilbao 2032 - CABA',
         },
         thumbnail: 'images/bilbao/thumbnail/thumbnail_01.jpg',
         logo: 'images/bilbao/logo/logo_01.png',
@@ -151,64 +148,96 @@ const PROPERTIES_DATA = [
         category: 'touch',
         status: 'en construccion',
         residence: {
+            title: 'Residencias',
             imagesUrl: [
-                'images/superi/residence/residence_01.jpg',
+                'images/superi/residence/residence_05.jpg',
                 'images/superi/residence/residence_02.jpg',
+                'images/superi/residence/residence_04.jpg',
+                'images/superi/residence/residence_01.jpg',
+                'images/superi/residence/residence_03.jpg',
             ],
             text: [
-                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Unidades desde 35 hasta 120 m2',
+                'Todas integradas a un estilo de placer. Para un nuevo concepto de calidad espacial. Departamentos 1, 2, 3 ambientes que inspiran a vivir mejor.',
             ],
         },
         amenities: {
+            title: 'Amenities',
             imagesUrl: [
                 'images/superi/amenities/amenities_01.jpg',
                 'images/superi/amenities/amenities_02.jpg',
             ],
             text: [
-                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Nuestro Sum es una extensión de nuestros momentos de placer. Gran terraza, parrilla y pileta rooftop. Alcanzá el verde de Belgrano R.',
             ],
         },
         location: {
             lat: -34.57375,
             lng: -58.46032,
-            address: 'Avenida del Libertador 5851, CABA',
+            address: 'Superí 1431 - CABA',
         },
         thumbnail: 'images/superi/thumbnail/thumbnail_01.jpg',
         logo: 'images/superi/logo/logo_01.png',
         blueprint: [
             {
-                name: 'piso_01_unidad_01',
-                // imageUrl:
-                //     'images/superi/blueprint/blueprint_piso_01_unidad_01.jpg',
+                name: 'piso_01-02_unidad_04',
                 imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
-                squareMeters: 44,
+                    'images/superi/blueprint/blueprint_piso_01-02_unidad_04_35,00m2.jpg',
+                squareMeters: 35.0,
             },
             {
-                name: 'piso_01_unidad_02',
+                name: 'piso_01-07_unidad_03',
                 imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
-                squareMeters: 48,
+                    'images/superi/blueprint/blueprint_piso_01-07_unidad_03_40,45m2.jpg',
+                squareMeters: 40.45,
             },
             {
-                name: 'piso_02_unidad_02',
+                name: 'piso_00_unidad_03',
                 imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
-                squareMeters: 56,
+                    'images/superi/blueprint/blueprint_piso_00_unidad_03_51,27m2.jpg',
+                squareMeters: 51.27,
             },
             {
-                name: 'piso_03_unidad_03',
+                name: 'piso_01-02_unidad_01',
                 imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
-                squareMeters: 78,
+                    'images/superi/blueprint/blueprint_piso_01-02_unidad_01_54,65m2.jpg',
+                squareMeters: 54.65,
             },
             {
-                name: 'piso_11_unidad_01',
+                name: 'piso_01-07_unidad_02',
                 imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
-                squareMeters: 90,
+                    'images/superi/blueprint/blueprint_piso_01-07_unidad_02_53,33m2.jpg',
+                squareMeters: 53.33,
+            },
+            {
+                name: 'piso_00_unidad_02',
+                imageUrl:
+                    'images/superi/blueprint/blueprint_piso_00_unidad_02_70,11m2.jpg',
+                squareMeters: 70.11,
+            },
+            {
+                name: 'piso_03_unidad_01',
+                imageUrl:
+                    'images/superi/blueprint/blueprint_piso_03_unidad_01_88,15m2.jpg',
+                squareMeters: 88.15,
+            },
+            {
+                name: 'piso_04-07_unidad_01',
+                imageUrl:
+                    'images/superi/blueprint/blueprint_piso_04-07_unidad_01_71,65m2.jpg',
+                squareMeters: 71.65,
+            },
+            {
+                name: 'piso_08_unidad_01',
+                imageUrl:
+                    'images/superi/blueprint/blueprint_piso_08_unidad_01_120,27m2.jpg',
+                squareMeters: 120.27,
+            },
+            {
+                name: 'piso_08_unidad_02',
+                imageUrl:
+                    'images/superi/blueprint/blueprint_piso_08_unidad_02_138,62m2.jpg',
+                squareMeters: 138.62,
             },
             // documents/superi/blueprint/superi_planos_35m2.pdf
         ],
@@ -222,29 +251,35 @@ const PROPERTIES_DATA = [
         category: 'touch',
         status: 'futuro lanzamiento',
         residence: {
+            title: 'Oficinas',
             imagesUrl: [
                 'images/libertador_office/residence/residence_01.jpg',
                 'images/libertador_office/residence/residence_02.jpg',
+                'images/libertador_office/residence/residence_03.jpg',
+                'images/libertador_office/residence/residence_04.jpg',
             ],
             text: [
-                'Residence, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Edificio corporativo AAA, Oficinas unificables.',
+                'Hall de doble altura, Locales Comerciales, Cuatro niveles de cocheras privadas.',
+                'Seguridad 24hs con control de acceso y CCTV',
+                'Ascensores de última generación con sistema de “llamada anticipada”.',
             ],
         },
         amenities: {
+            title: 'Amenities',
             imagesUrl: [
                 'images/libertador_office/amenities/amenities_01.jpg',
                 'images/libertador_office/amenities/amenities_02.jpg',
+                'images/libertador_office/amenities/amenities_03.jpg',
             ],
             text: [
-                'Amenities, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,consequatur totam? Neque est asperiores vero, nulla similiqueno strum non maxime. Labore consequuntur nobis sit molestiae consectetur totam quo aperiam dolorum.',
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Edificio con espacios comunes de calidad y elegancia, salón de usos múltiples, gimnasio equipado de última generación y laundry.',
             ],
         },
         location: {
             lat: -34.557967,
             lng: -58.44649,
-            address: 'Avenida del Libertador 5851, CABA',
+            address: 'Av. del Libertador 5851 - CABA',
         },
         thumbnail: 'images/libertador_office/thumbnail/thumbnail_01.jpg',
         logo: 'images/libertador_office/logo/logo_01.png',
