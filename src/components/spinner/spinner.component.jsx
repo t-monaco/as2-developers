b279c90b8aae51e2d3e5193e9db62e436f560b89
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-19 02:13:42 -0300

    Added spinner

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Spinner = () => (
+    <SpinnerOverlay>
+        <SpinnerContainer />
+    </SpinnerOverlay>
+);
+
+export default Spinner;
+
+// *** STYLES ***
+
+const SpinnerOverlay = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `;
+
+const SpinnerContainer = styled.div`
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    border: 3px solid rgba(195, 195, 195, 0.6);
+    border-radius: 50%;
+    border-top-color: #636767;
+    animation: spin 1s ease-in-out infinite;
+
+    -webkit-animation: spin 1s ease-in-out infinite;
+    @keyframes spin {
+        to {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+    @-webkit-keyframes spin {
+        to {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+`;
