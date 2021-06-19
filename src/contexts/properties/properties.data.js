1fd63138987bcc4e0364e5d02169d8e9c0a7b979
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-19 02:12:52 -0300

    Eddit context

--- src/contexts/properties/properties.data.js
+++ src/contexts/properties/properties.data.js
@@ -39,39 +39,12 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/sucre/thumbnail/thumbnail_01.jpg',
         logo: 'images/sucre/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Edificio con grandes expansiones individuales y vegetación vertical interpisos. Excelente relación entre interior y naturaleza. Departamentos 2 y 3 amb, Duplex, con parrilla propia',
+            ],
+        },
         blueprint: [
-            {
-                name: 'piso_01_unidad_01',
-                // imageUrl:
-                //     'images/sucre/blueprint/blueprint_piso_01_unidad_01.jpg',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
-                squareMeters: 44,
-            },
-            {
-                name: 'piso_01_unidad_02',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
-                squareMeters: 48,
-            },
-            {
-                name: 'piso_02_unidad_02',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
-                squareMeters: 56,
-            },
-            {
-                name: 'piso_03_unidad_03',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
-                squareMeters: 78,
-            },
-            {
-                name: 'piso_11_unidad_01',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
-                squareMeters: 90,
-            },
             // documents/sucre/blueprint/sucre_planos_35m2.pdf
         ],
     },
@@ -103,41 +76,12 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/bilbao/thumbnail/thumbnail_01.jpg',
         logo: 'images/bilbao/logo/logo_01.png',
-        blueprint: [
-            {
-                name: 'piso_01_unidad_01',
-                // imageUrl:
-                //     'images/bilbao/blueprint/blueprint_piso_01_unidad_01.jpg',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+44m',
-                squareMeters: 44,
-            },
-            {
-                name: 'piso_01_unidad_02',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
-                squareMeters: 48,
-            },
-            {
-                name: 'piso_02_unidad_02',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+03+UNIDAD+02+56m',
-                squareMeters: 56,
-            },
-            {
-                name: 'piso_03_unidad_03',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
-                squareMeters: 78,
-            },
-            {
-                name: 'piso_11_unidad_01',
-                imageUrl:
-                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
-                squareMeters: 90,
-            },
-            // documents/bilbao/blueprint/bilbao_planos_35m2.pdf
-        ],
+        project_details: {
+            description: [
+                'Diseño moderno y equipamiento de alta calida. Exclusivos  Dormitorios  con  gran  iluminación a frente - contrafrente y  ubicación  destacada.',
+            ],
+        },
+        blueprint: [],
     },
 
     {
@@ -150,15 +94,14 @@ const PROPERTIES_DATA = [
         residence: {
             title: 'Residencias',
             imagesUrl: [
-                'images/superi/residence/residence_05.jpg',
-                'images/superi/residence/residence_02.jpg',
                 'images/superi/residence/residence_04.jpg',
+                'images/superi/residence/residence_02.jpg',
                 'images/superi/residence/residence_01.jpg',
                 'images/superi/residence/residence_03.jpg',
             ],
             text: [
-                'Unidades desde 35 hasta 120 m2',
-                'Todas integradas a un estilo de placer. Para un nuevo concepto de calidad espacial. Departamentos 1, 2, 3 ambientes que inspiran a vivir mejor.',
+                'New life concept',
+                'Somos conscientes de que cada línea de nuestros diseños condensa los deseos, las proyecciones y la imaginación de quienes confían en nosotros',
             ],
         },
         amenities: {
@@ -168,7 +111,9 @@ const PROPERTIES_DATA = [
                 'images/superi/amenities/amenities_02.jpg',
             ],
             text: [
-                'Nuestro Sum es una extensión de nuestros momentos de placer. Gran terraza, parrilla y pileta rooftop. Alcanzá el verde de Belgrano R.',
+                'Amenities de gran calidad y confort.',
+                'Piscina + jacuzzi 20 m2 con impactantes vistas a Belgrano R.',
+                'Solarium + Lounge con parrilla + Laundry equipado.',
             ],
         },
         location: {
@@ -178,6 +123,13 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/superi/thumbnail/thumbnail_01.jpg',
         logo: 'images/superi/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Diseño elegante y funcional. Cada departamento de TOUCH SUPERI está diseñado cuidadosamente y equipado con instalaciones de primera categoría.',
+            ],
+            view_360:
+                'https://kuula.co/share/7t2Z9?fs=1&vr=0&sd=1&thumbs=1&info=1&logo=1',
+        },
         blueprint: [
             {
                 name: 'piso_01-02_unidad_04',
@@ -283,6 +235,11 @@ const PROPERTIES_DATA = [
         },
         thumbnail: 'images/libertador_office/thumbnail/thumbnail_01.jpg',
         logo: 'images/libertador_office/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Plantas amplias y versátiles que permiten flexibilizar los espacios de acuerdo a las necesidades.',
+            ],
+        },
         blueprint: [
             {
                 name: 'piso_01_unidad_01',
@@ -316,6 +273,290 @@ const PROPERTIES_DATA = [
             },
             // documents/libertador_office/blueprint/libertador_office_planos_35m2.pdf
         ],
+    },
+
+    {
+        id: uuid(),
+        name: 'LIBERTADOR RIVER',
+        // linkName: 'libertador_river',
+        heroImage: 'images/libertador_river/heroImage/heroImage_01.jpg',
+        category: 'touch',
+        status: 'en construccion',
+        residence: {
+            title: 'Residencias',
+            imagesUrl: [
+                'images/libertador_river/residence/residence_05.jpg',
+                'images/libertador_river/residence/residence_02.jpg',
+                'images/libertador_river/residence/residence_04.jpg',
+                'images/libertador_river/residence/residence_01.jpg',
+                'images/libertador_river/residence/residence_03.jpg',
+            ],
+            text: [
+                'Unidades desde 35 hasta 120 m2',
+                'Todas integradas a un estilo de placer. Para un nuevo concepto de calidad espacial. Departamentos 1, 2, 3 ambientes que inspiran a vivir mejor.',
+            ],
+        },
+        amenities: {
+            title: 'Amenities',
+            imagesUrl: [
+                'images/libertador_river/amenities/amenities_01.jpg',
+                'images/libertador_river/amenities/amenities_02.jpg',
+            ],
+            text: [
+                'Nuestro Sum es una extensión de nuestros momentos de placer. Gran terraza, parrilla y pileta rooftop. Alcanzá el verde de Belgrano R.',
+            ],
+        },
+        location: {
+            lat: -34.57375,
+            lng: -58.46032,
+            address: 'Superí 1431 - CABA',
+        },
+        thumbnail: 'images/libertador_river/thumbnail/thumbnail_01.jpg',
+        logo: 'images/libertador_river/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Plantas amplias y versátiles que permiten flexibilizar los espacios de acuerdo a las necesidades.',
+            ],
+        },
+        blueprint: [
+            {
+                name: 'piso_01-02_unidad_04',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_01-02_unidad_04_35,00m2.jpg',
+                squareMeters: 35.0,
+            },
+            {
+                name: 'piso_01-07_unidad_03',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_01-07_unidad_03_40,45m2.jpg',
+                squareMeters: 40.45,
+            },
+            {
+                name: 'piso_00_unidad_03',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_00_unidad_03_51,27m2.jpg',
+                squareMeters: 51.27,
+            },
+            {
+                name: 'piso_01-02_unidad_01',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_01-02_unidad_01_54,65m2.jpg',
+                squareMeters: 54.65,
+            },
+            {
+                name: 'piso_01-07_unidad_02',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_01-07_unidad_02_53,33m2.jpg',
+                squareMeters: 53.33,
+            },
+            {
+                name: 'piso_00_unidad_02',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_00_unidad_02_70,11m2.jpg',
+                squareMeters: 70.11,
+            },
+            {
+                name: 'piso_03_unidad_01',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_03_unidad_01_88,15m2.jpg',
+                squareMeters: 88.15,
+            },
+            {
+                name: 'piso_04-07_unidad_01',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_04-07_unidad_01_71,65m2.jpg',
+                squareMeters: 71.65,
+            },
+            {
+                name: 'piso_08_unidad_01',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_08_unidad_01_120,27m2.jpg',
+                squareMeters: 120.27,
+            },
+            {
+                name: 'piso_08_unidad_02',
+                imageUrl:
+                    'images/libertador_river/blueprint/blueprint_piso_08_unidad_02_138,62m2.jpg',
+                squareMeters: 138.62,
+            },
+            // documents/superi/blueprint/superi_planos_35m2.pdf
+        ],
+    },
+
+    {
+        id: uuid(),
+        name: 'LIBERTADOR GOLF',
+        // linkName: 'libertador_golf',
+        heroImage: 'images/libertador_golf/heroImage/heroImage_01.jpg',
+        category: 'touch',
+        status: 'futuro lanzamiento',
+        residence: {
+            title: 'Oficinas',
+            imagesUrl: [
+                'images/libertador_golf/residence/residence_01.jpg',
+                'images/libertador_golf/residence/residence_02.jpg',
+                'images/libertador_golf/residence/residence_03.jpg',
+                'images/libertador_golf/residence/residence_04.jpg',
+            ],
+            text: [
+                'Edificio corporativo AAA, Oficinas unificables.',
+                'Hall de doble altura, Locales Comerciales, Cuatro niveles de cocheras privadas.',
+                'Seguridad 24hs con control de acceso y CCTV',
+                'Ascensores de última generación con sistema de “llamada anticipada”.',
+            ],
+        },
+        amenities: {
+            title: 'Amenities',
+            imagesUrl: [
+                'images/libertador_golf/amenities/amenities_01.jpg',
+                'images/libertador_golf/amenities/amenities_02.jpg',
+                'images/libertador_golf/amenities/amenities_03.jpg',
+            ],
+            text: [
+                'Edificio con espacios comunes de calidad y elegancia, salón de usos múltiples, gimnasio equipado de última generación y laundry.',
+            ],
+        },
+        location: {
+            lat: -34.557967,
+            lng: -58.44649,
+            address: 'Av. del Libertador 5851 - CABA',
+        },
+        thumbnail: 'images/libertador_golf/thumbnail/thumbnail_01.jpg',
+        logo: 'images/libertador_golf/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Plantas amplias y versátiles que permiten flexibilizar los espacios de acuerdo a las necesidades.',
+            ],
+        },
+        blueprint: [
+            {
+                name: 'piso_01_unidad_01',
+                imageUrl:
+                    'images/libertador_golf/blueprint/blueprint_piso_01_unidad_03.jpg',
+                squareMeters: 44,
+            },
+            {
+                name: 'piso_01_unidad_02',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+48m',
+                squareMeters: 48,
+            },
+            {
+                name: 'piso_02_unidad_02',
+                imageUrl:
+                    'images/libertador_golf/blueprint/blueprint_piso_01_unidad_03.jpg',
+                squareMeters: 56,
+            },
+            {
+                name: 'piso_03_unidad_03',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+78m',
+                squareMeters: 78,
+            },
+            {
+                name: 'piso_11_unidad_01',
+                imageUrl:
+                    'https://via.placeholder.com/700x700?text=PISO+01+UNIDAD+01+90m',
+                squareMeters: 90,
+            },
+            // documents/libertador_golf/blueprint/libertador_golf_planos_35m2.pdf
+        ],
+    },
+
+    {
+        id: uuid(),
+        name: 'OCEAN BEACH',
+        // linkName: 'ocean_beach',
+        heroImage: 'images/ocean_beach/heroImage/heroImage_01.jpg',
+        category: 'touch',
+        status: 'futuro lanzamiento',
+        residence: {
+            title: 'Oficinas',
+            imagesUrl: [
+                'images/ocean_beach/residence/residence_01.jpg',
+                'images/ocean_beach/residence/residence_02.jpg',
+                'images/ocean_beach/residence/residence_03.jpg',
+                'images/ocean_beach/residence/residence_04.jpg',
+            ],
+            text: [
+                'Edificio corporativo AAA, Oficinas unificables.',
+                'Hall de doble altura, Locales Comerciales, Cuatro niveles de cocheras privadas.',
+                'Seguridad 24hs con control de acceso y CCTV',
+                'Ascensores de última generación con sistema de “llamada anticipada”.',
+            ],
+        },
+        amenities: {
+            title: 'Amenities',
+            imagesUrl: [
+                'images/ocean_beach/amenities/amenities_01.jpg',
+                'images/ocean_beach/amenities/amenities_02.jpg',
+                'images/ocean_beach/amenities/amenities_03.jpg',
+            ],
+            text: [
+                'Edificio con espacios comunes de calidad y elegancia, salón de usos múltiples, gimnasio equipado de última generación y laundry.',
+            ],
+        },
+        location: {
+            lat: -34.557967,
+            lng: -58.44649,
+            address: 'Av. del Libertador 5851 - CABA',
+        },
+        thumbnail: 'images/ocean_beach/thumbnail/thumbnail_01.jpg',
+        logo: 'images/ocean_beach/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Plantas amplias y versátiles que permiten flexibilizar los espacios de acuerdo a las necesidades.',
+            ],
+        },
+        blueprint: [],
+    },
+
+    {
+        id: uuid(),
+        name: 'BAL HARBOUR',
+        // linkName: 'bal_harbour',
+        heroImage: 'images/bal_harbour/heroImage/heroImage_01.jpg',
+        category: 'touch',
+        status: 'futuro lanzamiento',
+        residence: {
+            title: 'Oficinas',
+            imagesUrl: [
+                'images/bal_harbour/residence/residence_01.jpg',
+                'images/bal_harbour/residence/residence_02.jpg',
+                'images/bal_harbour/residence/residence_03.jpg',
+                'images/bal_harbour/residence/residence_04.jpg',
+            ],
+            text: [
+                'Edificio corporativo AAA, Oficinas unificables.',
+                'Hall de doble altura, Locales Comerciales, Cuatro niveles de cocheras privadas.',
+                'Seguridad 24hs con control de acceso y CCTV',
+                'Ascensores de última generación con sistema de “llamada anticipada”.',
+            ],
+        },
+        amenities: {
+            title: 'Amenities',
+            imagesUrl: [
+                'images/bal_harbour/amenities/amenities_01.jpg',
+                'images/bal_harbour/amenities/amenities_02.jpg',
+                'images/bal_harbour/amenities/amenities_03.jpg',
+            ],
+            text: [
+                'Edificio con espacios comunes de calidad y elegancia, salón de usos múltiples, gimnasio equipado de última generación y laundry.',
+            ],
+        },
+        location: {
+            lat: -34.557967,
+            lng: -58.44649,
+            address: 'Av. del Libertador 5851 - CABA',
+        },
+        thumbnail: 'images/bal_harbour/thumbnail/thumbnail_01.jpg',
+        logo: 'images/bal_harbour/logo/logo_01.png',
+        project_details: {
+            description: [
+                'Plantas amplias y versátiles que permiten flexibilizar los espacios de acuerdo a las necesidades.',
+            ],
+        },
+        blueprint: [],
     },
 ];
 export default PROPERTIES_DATA;
