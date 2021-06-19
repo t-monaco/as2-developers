a9de43922a4abf3c9b79216cb9388d165a6be443
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-19 02:17:37 -0300

    WIP - multiple updates

--- src/components/blueprint/blueprint.component.jsx
+++ src/components/blueprint/blueprint.component.jsx
@@ -4,7 +4,7 @@ import SectionTitle from '../section-title/section-title.component';
 import SlickSlider from '../slick-slider/slick-slider.component';
 import './blueprint.styles.scss';
 
-const Blueprint = ({ blueprint, linkName, name }) => {
+const Blueprint = ({ blueprint, linkName, name, project_details }) => {
     const sliderSettings = {
         autoplay: true,
         arrows: true,
@@ -47,41 +47,66 @@ const Blueprint = ({ blueprint, linkName, name }) => {
         <div className='blueprint'>
             <div className='blueprint-content'>
                 <div className='title-description'>
-                    <SectionTitle type='h2' mainTitle='PLANTAS' />
-                    <p className='description'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Laboriosam quam doloribus amet voluptates quod explicabo
-                        accusantium aliquid!
-                    </p>
+                    <SectionTitle type='h2' mainTitle='PROJECTO' />
+                    <p className='description'>{project_details.description}</p>
                 </div>
-                <div className='input-container'>
-                    {filterOptions.map((value, key) => (
-                        <div className='input-group' key={key}>
-                            <input
-                                id={key}
-                                type='radio'
-                                value={value}
-                                onChange={handleChange}
-                                checked={squareMeterFilter === value}
-                            />
-                            <label htmlFor={key}>Desde {value} m2</label>
-                        </div>
-                    ))}
+                <div className='downloadable-input-container'>
+                    <a
+                        className='downloadable-input'
+                        href={`documents/${linkName}/blueprint/${linkName}_proyecto.pdf`}
+                        download={`Proyecto - ${name}`}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        Descargar projecto
+                    </a>
+                    {project_details.view_360 && (
+                        <a
+                            className='downloadable-input'
+                            href={project_details.view_360}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            Vista 360
+                        </a>
+                    )}
                 </div>
-                <a
-                    className='blueprint-download-pdf'
-                    href={`documents/${linkName}/blueprint/${linkName}_planos_${squareMeterFilter}m2.pdf`}
-                    download={`Planos ${name} - ${squareMeterFilter} m2`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Descargar plantas
-                </a>
+                {blueprint.length > 0 && (
+                    <div className='input-container'>
+                        {filterOptions.map((value, key) => (
+                            <div className='input-group' key={key}>
+                                <input
+                                    id={key}
+                                    type='radio'
+                                    value={value}
+                                    onChange={handleChange}
+                                    checked={squareMeterFilter === value}
+                                />
+                                <label htmlFor={key}>Desde {value} m2</label>
+                            </div>
+                        ))}
+                    </div>
+                )}
+                <div className='downloadable-input-container'>
+                    {blueprint.length > 0 && (
+                        <a
+                            className='downloadable-input floors-btn'
+                            href={`documents/${linkName}/blueprint/${linkName}_planos_${squareMeterFilter}m2.pdf`}
+                            download={`Planos ${name} - ${squareMeterFilter} m2`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            Descargar plantas
+                        </a>
+                    )}
+                </div>
             </div>
-            <SlickSlider
-                settings={sliderSettings}
-                images={filterBlueprints.map((floor) => floor.imageUrl)}
-            />
+            {blueprint.length > 0 && (
+                <SlickSlider
+                    settings={sliderSettings}
+                    images={filterBlueprints.map((floor) => floor.imageUrl)}
+                />
+            )}
             <Divider />
         </div>
     );
