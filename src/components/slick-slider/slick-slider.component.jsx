8ff476757e2f28e4bcd6a200b6a9beaeffba4cce
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-06-28 10:28:49 -0300

    Slider images

--- src/components/slick-slider/slick-slider.component.jsx
+++ src/components/slick-slider/slick-slider.component.jsx
@@ -9,10 +9,25 @@ import slideImg01 from './../../assets/slider/slide_01.jpg';
 import slideImg02 from './../../assets/slider/slide_02.jpg';
 import slideImg03 from './../../assets/slider/slide_03.jpg';
 import slideImg04 from './../../assets/slider/slide_04.jpg';
+import slideImg05 from './../../assets/slider/slide_05.jpg';
+import slideImg06 from './../../assets/slider/slide_06.jpg';
+import slideImg07 from './../../assets/slider/slide_07.jpg';
+import slideImg08 from './../../assets/slider/slide_08.jpg';
+import slideImg09 from './../../assets/slider/slide_09.jpg';
 
 const SlickSlider = ({ images, settings }) => {
     if (!images) {
-        images = [slideImg01, slideImg02, slideImg03, slideImg04];
+        images = [
+            slideImg01,
+            slideImg02,
+            slideImg03,
+            slideImg04,
+            slideImg05,
+            slideImg06,
+            slideImg07,
+            slideImg08,
+            slideImg09,
+        ];
     }
 
     return (
